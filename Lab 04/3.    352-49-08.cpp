1918601c325f1ab14d5a1b92413e8a103ef6b619
Maruf Ahmed Santo <110012173+santo352@users.noreply.github.com>
2022-08-15 20:57:02 +0600

    Update 3.    352-49-08.cpp

--- Lab 04/3.    352-49-08.cpp
+++ Lab 04/3.    352-49-08.cpp
@@ -1,1 +1,14 @@
+//problem no.281A
+//problem name:Word Capitalization
 
+#include<iostream>
+using namespace std;
+int main()
+{
+  string str;
+  cin>>str;
+
+   str[0]=toupper(str[0]);
+   cout<<str<<endl;
+   return 0;
+}
