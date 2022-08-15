92b347da346ae8edf2092db475dd9db2dabda61a
Maruf Ahmed Santo <110012173+santo352@users.noreply.github.com>
2022-08-15 20:03:31 +0600

    Update 1.    352-49-08.cpp

--- Lab 02/1.    352-49-08.cpp
+++ Lab 02/1.    352-49-08.cpp
@@ -1,1 +1,22 @@
+//problem  name:Team
 
+// problem  no:231A
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int m,x,y,z,count=0;
+    cin>>m;
+    for(int i=0; i<m; i++)
+    {
+        cin>>x>>y>>z;
+        if( x+y+z >=2 )
+        {
+            count++;
+        }
+    }
+    cout<<count<<endl;
+  getch();
+}
