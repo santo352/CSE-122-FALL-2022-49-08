7b8cac5e0ec82351ef488bea1c1515bf67deca35
Maruf Ahmed Santo <110012173+santo352@users.noreply.github.com>
2022-08-15 20:25:22 +0600

    Update 3.    352-49-08.cpp

--- Lab 02/3.    352-49-08.cpp
+++ Lab 02/3.    352-49-08.cpp
@@ -1,1 +1,26 @@
+//problem  name:Next Round 
 
+// problem  no:158A
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+int n, k, x, y = 0, mas[10000];
+int main()
+{
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) {
+        cin >> x;
+        mas[i] = x;
+    }
+    for (int j = 1; j <= n; j++) {
+      
+    if (mas[j] >= mas[k] && mas[j] > 0)
+        {
+            y++;
+        }
+    }
+    cout << y;
+  
+  getch ();
+}
