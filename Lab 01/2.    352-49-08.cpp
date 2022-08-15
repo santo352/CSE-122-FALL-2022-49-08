8b335229cf1fa6c74e009fa0cd8ebe3cc3b3c9a0
Maruf Ahmed Santo <110012173+santo352@users.noreply.github.com>
2022-08-15 19:42:23 +0600

    Update 2.    352-49-08.cpp

--- Lab 01/2.    352-49-08.cpp
+++ Lab 01/2.    352-49-08.cpp
@@ -1,1 +1,24 @@
+//CodeForces Problem Name: Way too long words
+//Problem Code: 71A
 
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int t, i;
+    string word;
+
+    cin>>t;
+    for(i = 1; i <= t; i++){
+        cin>>word;
+        if(word.length() > 10){
+            cout << word[0] << word.length() - 2 << word[word.length() - 1] <<endl;
+        }
+        else{
+            cout << word << endl;
+        }
+    }
+    return 0;
+}
