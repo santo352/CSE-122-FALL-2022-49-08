48f2b4fd5fe6feff44aa16c37705b8f2468837f5
Maruf Ahmed Santo <110012173+santo352@users.noreply.github.com>
2022-08-15 19:38:14 +0600

    Update 1.    352-49-08.cpp

--- Lab 01/1.    352-49-08.cpp
+++ Lab 01/1.    352-49-08.cpp
@@ -1,1 +1,18 @@
+//CodeForces Problem Name: Watermelon
+//Problem Code: 4A
 
+#include
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+int w;
+cin>>w;
+if(w % 2 == 0){
+cout << "YES" << endl;
+} else{
+cout << "NO" << endl;
+}
+getech ();
+}
