ea06654a5865aa2b67429509fb0397483fde08bb
Maruf Ahmed Santo <110012173+santo352@users.noreply.github.com>
2022-08-15 20:15:12 +0600

    Update 2.    352-49-08.cpp

--- Lab 02/2.    352-49-08.cpp
+++ Lab 02/2.    352-49-08.cpp
@@ -1,1 +1,19 @@
+//problem  name:Theatre Square
 
+// problem  no:1A
+
+
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+long long n, m, a;
+
+cin>>n>>m>>a;
+
+cout<<(((n+a-1)/a)*((m+a-1)/a));
+
+getch ();
+}
