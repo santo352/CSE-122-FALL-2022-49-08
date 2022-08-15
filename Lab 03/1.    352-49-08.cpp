6bad6d65bd219cb622bb30f9f10e852c8eee46c7
Maruf Ahmed Santo <110012173+santo352@users.noreply.github.com>
2022-08-15 20:30:27 +0600

    Update 1.    352-49-08.cpp

--- Lab 03/1.    352-49-08.cpp
+++ Lab 03/1.    352-49-08.cpp
@@ -1,1 +1,15 @@
+//problem name:Domino piling
 
+//problem no.50A
+
+#include <iostream>
+#include<conio.h>
+using namespace std;
+ 
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << x * y / 2 << endl;
+    getch ();
+}
