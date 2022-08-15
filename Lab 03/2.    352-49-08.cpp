a451a6113fb6e93c7b2b6503f3b933de300a6590
Maruf Ahmed Santo <110012173+santo352@users.noreply.github.com>
2022-08-15 20:37:04 +0600

    Update 2.    352-49-08.cpp

--- Lab 03/2.    352-49-08.cpp
+++ Lab 03/2.    352-49-08.cpp
@@ -1,1 +1,32 @@
+//problem name:Bit++
 
+//problem no.282A
+
+#include <iostream>
+#include<conio.h>
+#include <string>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n, x(0);
+    cin >> n;
+ 
+    string s;
+    while (n--)
+    {
+        cin >> s;
+        if (s[1] == '+')
+        {
+            ++x;
+        }
+        else
+        {
+            --x;
+        }
+    }
+ 
+    cout << x << endl;
+    getch;
+}
