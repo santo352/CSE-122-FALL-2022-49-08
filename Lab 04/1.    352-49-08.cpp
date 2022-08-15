12e331416f6ee56bc1decc510fb0fbdc81fecb98
Maruf Ahmed Santo <110012173+santo352@users.noreply.github.com>
2022-08-15 20:49:35 +0600

    Update 1.    352-49-08.cpp

--- Lab 04/1.    352-49-08.cpp
+++ Lab 04/1.    352-49-08.cpp
@@ -1,1 +1,27 @@
+//problem no.112A
+//problem name:Petya Strings
 
+#include<iostream>
+#include<conio.h>
+using namespace std;
+string a, b;
+int main()
+{
+    cin >> a >> b;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] < 92) {
+            a[i] += 32;
+        }
+        if (b[i] < 92) {
+            b[i] += 32;
+        }
+    }
+    if (a < b) {
+        cout << -1;
+    } else if (a > b) {
+        cout << 1;
+    } else if (a == b) {
+        cout << 0;
+    }
+  getch ();
+}
